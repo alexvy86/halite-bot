e102fea6bea56139653e00ef56f4c6e4842bdc97
Alejandro Villarreal <alex@alexvy86.com>
2016-12-03 15:41:24 -0600

    Added pylint and fixed recommendations

--- MyBot.py
+++ MyBot.py
@@ -1,51 +1,60 @@
+"""My Halite Bot"""
+
 import logging, logging.config
-from hlt import *
-from networking import *
+from hlt import * # pylint: disable=W0401,W0614
+from networking import * # pylint: disable=W0401,W0614
 import util
 
-def move(location):
-    site = gameMap.getSite(location)
+def move(location, game_map):
+    """Defines the move logic for the bot"""
+    site = game_map.getSite(location)
 
     # This strategy prioritizes expansion
-    
+
     # Conquer neighbors that can be conquered
-    for d in CARDINALS:
-        neighbour_site = gameMap.getSite(location, d)
-        if neighbour_site.owner != myID and neighbour_site.strength < site.strength:
-            return Move(location, d)
-    
+    for direction in CARDINALS:
+        neighbour_site = game_map.getSite(location, direction)
+        if neighbour_site.owner != MY_ID and neighbour_site.strength < site.strength:
+            return Move(location, direction)
+
     # Do not stay still if the site's production will cause strength to go past the cap (255)
     if site.strength + site.production > 255:
-        return Move(location, NORTH if random.random() > 0.5 else WEST) 
+        return Move(location, NORTH if random.random() > 0.5 else WEST)
 
     # Stay still until strength is 5 times the site's production
     if site.strength < site.production * 5:
-        return Move(location, STILL)    
+        return Move(location, STILL)
 
     return Move(location, STILL)
 
-myID, gameMap = getInit()
+def main(bot_id, game_map):
+    """Main logic of the bot"""
+
+    # Initialize logging
+    logging.config.fileConfig("logging_config.ini")
+
+    # Get maps
+    prod_map = []
+    strength_map = []
+    for y in range(game_map.height): # pylint: disable=C0103
+        prod_row = [game_map.getSite(Location(x, y)).production for x in range(game_map.width)]
+        prod_map.append(prod_row)
+        strength_row = [game_map.getSite(Location(x, y)).strength for x in range(game_map.width)]
+        strength_map.append(strength_row)
+    logging.info("PRODUCTION MAP\r\n" + util.formatMatrix(prod_map))
+    logging.info("STRENGTH MAP\r\n" + util.formatMatrix(strength_map))
+
+    while True:
+        moves = []
+        game_map = getFrame()
+        for y in range(game_map.height): # pylint: disable=C0103
+            for x in range(game_map.width): # pylint: disable=C0103
+                loc = Location(x, y)
+                if game_map.getSite(loc).owner == bot_id:
+                    moves.append(move(loc, game_map))
+        sendFrame(moves)
+
+MY_ID, GAME_MAP = getInit()
 sendInit("MyPythonBot")
 
-# Initialize logging
-logs_directory = "logs" 
-logging.config.fileConfig("logging_config.ini")
-
-# Get maps
-prodMap = []
-strengthMap = []
-for y in range(gameMap.height):
-    prodMap.append([gameMap.getSite(Location(x, y)).production for x in range(gameMap.width)])
-    strengthMap.append([gameMap.getSite(Location(x, y)).strength for x in range(gameMap.width)])
-logging.info("PRODUCTION MAP\r\n" + util.formatMatrix(prodMap))
-logging.info("STRENGTH MAP\r\n" + util.formatMatrix(strengthMap))
-
-while True:
-    moves = []
-    gameMap = getFrame()
-    for y in range(gameMap.height):
-        for x in range(gameMap.width):
-            location = Location(x, y)
-            if gameMap.getSite(location).owner == myID:
-                moves.append(move(location))
-    sendFrame(moves)
+main(MY_ID, GAME_MAP)
